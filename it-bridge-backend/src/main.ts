9ee75abd60caebef14d2a2c517437212d8027c77
AlexePaul <64796131+AlexePaul@users.noreply.github.com>
2026-01-12 16:28:23 +0200

    Made Backend Listen on 0.0.0.0

--- it-bridge-backend/src/main.ts
+++ it-bridge-backend/src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
         credentials: true,
     });
 
-    await app.listen(process.env.PORT ?? 3000);
+    const port = Number(process.env.PORT ?? 3000);
+    await app.listen(port, '0.0.0.0');
+    console.log(`Server listening on http://0.0.0.0:${port}`);
 }
 bootstrap();
